32c55ba6257ba700ba76c808ce9de4ed1a0a84c6
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-26 15:49:21 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 import Child from "./Child";
@@ -6,35 +5,42 @@ import Child from "./Child";
 const todoArr = [
   {
     id: 1,
-    todo:"Learn React",
+    todo: "Learn React",
     completed: false
   },
   {
-    id:2,
-    todo:"Build a React App",
+    id: 2,
+    todo: "Build a React App",
     completed: false
   },
   {
-    id:3,
-    todo:"Deploye the React App",
+    id: 3,
+    todo: "Deploy the React App",
     completed: false
   }
 ];
 
 const App = () => {
-  const [todos, setTodos] = useState(todoArr) 
-  function handleComplete(id){
-    setTodos(todos => {
-      const newTodos = [...todos];
-      newTodos[id-1].completed = true;
+  const [todos, setTodos] = useState(todoArr);
+
+  function handleComplete(id) {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        // Find the todo with the same id and update the completed field
+        if (todo.id === id) {
+          return { ...todo, completed: true };
+        }
+        return todo;
+      });
       return newTodos;
     });
   }
+
   return (
     <div>
-        <Child todos={todos} handleComplete={handleComplete}/>
+      <Child todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-const Child = ({todos, handleComplete}) => {
-    
+const Child = ({ todos, handleComplete }) => {
   return (
     <div>
       <ul>
-        {todos.map((todo, index) => {
-            return <li key={index+1}>{todo.todo}{!todo.completed && <button onClick={()=>{
-                handleComplete(todo.id);
-            }}>Complete</button>}</li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.todo}
+              {!todo.completed && (
+                <button onClick={() => handleComplete(todo.id)}>
+                  Complete
+                </button>
+              )}
+            </li>
+          );
         })}
       </ul>
     </div>
   );
-}
+};
 
 export default Child;
